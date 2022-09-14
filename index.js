98a0884e438d273d5144b59b2749832f9f0d6878
Jon Mitchell <jon@altinc.ca>
2022-09-14 20:40:06 -0230

    refine

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 ////////////////////////////////////////
 /////
-///// SIP Options Health Check
+///// node SIP check
 /////
 ///// Jon Mitchell - September 2022
 ///// Alt Telecom 
@@ -12,59 +12,71 @@ require = require("esm")(module/* , options */)
 
 module.exports = require("./main.js")
 
-const { performance } = require('perf_hooks');
-const NetcatClient = require('netcat/client')
-
 const http = require('http')
 const url = require('url');
+const { performance } = require('perf_hooks');
 
+const NetcatClient = require('netcat/client')
 var nc = new NetcatClient()
-
-function onReady() {
-    startTime = performance.now()
-}
-function sipCheck(option, server) {
-    nc.udp()
-        .port(5060)
-        .wait(1000)
-        .init()
-        .send(option, server)
-        .on('ready', onReady)
-        .on('data', function (res) {
-            endTime = performance.now()
-            console.log(res.data.toString())
-            let regexResult = res.data.toString().search(/^(SIP\/2.0)/)
-            if (regexResult === 0) {
-                data += `<pingdom_http_custom_check>
-                <status>OK</status>
-                <response_time>${endTime - startTime}</response_time>
-                </pingdom_http_custom_check>`;
-                response.header("Content-Type", "application/xml");
-                response.status(200).send(data);
-            } else {
-                data += `<pingdom_http_custom_check>
-                <status>FAIL</status>
-                <response_time>${endTime - startTime}</response_time>
-                </pingdom_http_custom_check>`;
-                response.header("Content-Type", "application/xml");
-                response.status(200).send(data);
-            }
-        })
-}
 
 const app = http.createServer((request, response) => {
     const queryObject = url.parse(request.url, true).query;
     let server = queryObject['SBC']
-    var source = "159.2.121.134"
-    var option = `OPTIONS sip:${server} SIP/2.0
-    Via: SIP/2.0/UDP ${source}
-    From: <sip:monitoring@1${source}>
-    To: sip:monitoring@${server}
-    Call-ID: monitoring@${source}
-    CSeq: 1 OPTIONS
-    `
-    sipCheck(option, server)
 
+    if(server == null || server == '') {
+        server = "inbound-sbc1.tor.apbx.ca"
+    }
+
+    try {
+        var source = "159.2.121.134"
+        var option = `OPTIONS sip:${server} SIP/2.0
+Via: SIP/2.0/UDP ${source}
+From: <sip:monitoring@1${source}>
+To: sip:monitoring@${server}
+Call-ID: monitoring@${source}
+CSeq: 1 OPTIONS
+`
+        function onReady() {
+            startTime = performance.now()
+        }
+        function sipCheck(option) {
+            nc.udp()
+                .port(5060)
+                .wait(1000)
+                .init()
+                .send(option, server)
+                .on('ready', onReady)
+                .on('data', function (res) {
+                    endTime = performance.now()
+                    // console.log(res.data.toString())
+                    let regexResult = res.data.toString().search(/^(SIP\/2.0)/)
+                    if (regexResult === 0) {
+                        let responseTime = endTime - startTime
+                        // console.log(`Call to doSomething took ${responseTime.toFixed(3)} milliseconds`)
+                        data = `<pingdom_http_custom_check>
+                <status>OK</status>
+                <response_time>${responseTime.toFixed(3)}</response_time>
+                </pingdom_http_custom_check>`
+                        response.writeHead(200, { 'Content-Type': 'application/xml' })
+                        response.end(data)
+                    } else {
+                        data = `<pingdom_http_custom_check>
+                <status>NOTOK</status>
+                <response_time>${responseTime.toFixed(3)}</response_time>
+                </pingdom_http_custom_check>`
+                        response.writeHead(200, { 'Content-Type': 'application/xml' })
+                        response.end(data)
+                    }
+                })
+        }
+
+        sipCheck(option)
+
+    }
+    catch (err) {
+        response.writeHead(503, { 'Content-Type': 'application/xml' })
+        response.end("Service Unavailable: " + err)
+    }
 })
 
 const PORT = 3002
